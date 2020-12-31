4f939e651ad68b7b617e41858619784af7534eb4
240 <wow9176@naver.com>
2020-12-31 19:12:41 +0900

    1231

---
+++ 1231-1.py
@@ -0,0 +1,4 @@
+name = input()
+birth_mon = int(input())
+birth_day = int(input())
+print("나의 이름은 %s이고, 생일은 %d월 %d입니다." %(name, birth_mon, birth_day))
---
+++ 1231-2.py
@@ -0,0 +1,5 @@
+alkaline_earth_metals = [ 4, 12, 38, 56, 88 ]
+alkaline_earth_metals.index(88)
+alkaline_earth_metals.append(20)
+alkaline_earth_metals.sort()
+alkaline_earth_metals
